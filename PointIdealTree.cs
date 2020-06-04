16dc44b8f83c14c85713863f55e0bb01d343d6c2
IGShilyaev <55237654+IGShilyaev@users.noreply.github.com>
2020-06-05 00:43:01 +0500

    Add files via upload

---
+++ PointIdealTree.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PractTask10
+{
+    public class PointIdealTree<T>
+    {
+        public T data;
+        public PointIdealTree<T> left;
+        public PointIdealTree<T> right;
+
+        public PointIdealTree(T d)
+        {
+            data = d;
+            left = null;
+            right = null;
+        }
+
+        public PointIdealTree()
+        {
+            data = default(T);
+            left = null;
+            right = null;
+        }
+
+        public override string ToString()
+        {
+            return data.ToString() + " ";
+        }
+
+    }
+}
